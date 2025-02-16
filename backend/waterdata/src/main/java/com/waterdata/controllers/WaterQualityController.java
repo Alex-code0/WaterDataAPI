d952b09fabecc12068f89e61c5732b2d5b58e1f0
Alex-code0 <shadowsharky39@gmail.com>
2025-02-16 18:26:31 +0200

    Returning water data to the frontend

--- backend/waterdata/src/main/java/com/waterdata/controllers/WaterQualityController.java
+++ backend/waterdata/src/main/java/com/waterdata/controllers/WaterQualityController.java
@@ -31,12 +31,25 @@ public class WaterQualityController {
     private AnalysisService analysisService;
 
     public String getStationName(String stationId, JsonNode stationsNodes) {
-        for (JsonNode node : stationsNodes) {
-            if (node.has("MOScode") && node.get("MOScode").asText().equals(stationId)) {
+        for(JsonNode node : stationsNodes) {
+            if(node.has("MOScode") && node.get("MOScode").asText().equals(stationId)) {
                 return node.get("HYDWNAME").asText();
             }
         }
         return "Station not found";
+    }
+
+    public HashMap<String, Object> getParameterData(String parameterId, JsonNode parametersNodes, String value, String analysisDate) {
+        HashMap<String, Object> parameterData = new HashMap<>();
+        for(JsonNode node : parametersNodes) {
+            if(node.has("MOPACODE") && node.get("MOPACODE").asText().equals(parameterId)) {
+                parameterData.put("parameterName", node.get("MOPANAME").asText());
+                parameterData.put("parameterUnit", node.get("MOPAUNIT").asText());
+                parameterData.put("parameterValue", value);
+                parameterData.put("analysisDate", analysisDate);
+            }
+        }
+        return parameterData;
     }
 
     @GetMapping("/waterquality")
@@ -47,8 +60,20 @@ public class WaterQualityController {
             JsonNode analysisNodes = analysisService.fetchWaterData(station, year);
 
             HashMap<String, Object> response = new HashMap<>();
+            HashMap<String, HashMap<String, Object>> analysis = new HashMap<>();
+
+            for (JsonNode node : analysisNodes) {
+                String parameterId = node.get("ANMOPACODE").asText();
+                HashMap<String, Object> parameterData = getParameterData(parameterId, parametersNodes, node.get("MOANVALUE").asText(), node.get("MOANDATE").asText());
+
+                analysis.put(parameterId, parameterData);
+            }
+
             response.put("stationName", getStationName(station, stationsNodes));
-        } catch(Exception e) {
+            response.put("waterData", analysis);
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(500).body("Error: " + e.getMessage());
         }
